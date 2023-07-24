f5bdd5c21ec418c8269f3d2b5bd0f817348410b3
J-Miggy <jose.parami@lsu.edu.ph>
2023-07-24 19:24:00 +0800

    website

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal({
  //multiple text typed js
 
  const typed = new Typed('.multiple-text', {
-    strings: ['Programmer', 'UX/UI Designer'],
+    strings: ['Programmer', 'Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
