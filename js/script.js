38a8fa2521d0f61fbfa66ce7c9cb7f6d53439aff
josemiggy <jose@clickmind.ai>
2024-05-13 13:05:07 +0800

    Updated resume & skills

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal({
  //multiple text typed js
 
  const typed = new Typed('.multiple-text', {
-    strings: ['Software Developer', 'Designer'],
+    strings: ['Software Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
